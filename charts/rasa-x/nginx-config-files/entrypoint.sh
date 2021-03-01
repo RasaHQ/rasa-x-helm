9fd34cde1b751278fcb00c9154182b17ff408197
melindaloubser1 <melinda.loubser@gmail.com>
2021-03-01 10:51:55 +0100

    nginx entrypoint fix permissions

--- charts/rasa-x/nginx-config-files/entrypoint.sh
+++ charts/rasa-x/nginx-config-files/entrypoint.sh
@@ -4,21 +4,13 @@
 # https://github.com/bitnami/bitnami-docker-nginx/blob/483f0abb3ee3dbaff57ea154cb5c10ef5e064111/1.17/debian-10/Dockerfile#L35
 /opt/bitnami/scripts/nginx/entrypoint.sh
 
-# Set environment variables with default values if empty
-export RASA_X_HOST=${RASA_X_HOST:-"rasa-x:5002"}
-export RASA_PRODUCTION_HOST=${RASA_PRODUCTION_HOST:-"rasa-production:5005"}
-
-# Replace environment variable with given environment variables
-VARS_TO_REPLACE='$RASA_X_HOST:$RASA_PRODUCTION_HOST'
-envsubst "$VARS_TO_REPLACE" < /opt/bitnami/nginx/conf/conf.d/rasax.nginx > /opt/bitnami/nginx/conf/conf.d/rasax.nginx.tmp
-mv /opt/bitnami/nginx/conf/conf.d/rasax.nginx.tmp /opt/bitnami/nginx/conf/conf.d/rasax.nginx
-
-
 if [[ ! -f /opt/bitnami/nginx/conf/bitnami/terms/agree.txt ]]
 then
   echo "you have not agreed to the terms and conditions, Rasa X cannot start. To accept the terms and conditions run echo \"\$\{USER\} \$\(date\)\" > /opt/bitnami/nginx/conf/bitnami/terms/agree.txt on the host machine. You can find the terms at https://storage.googleapis.com/rasa-x-releases/rasa_x_ce_license_agreement.pdf"
   exit 1
 fi
+
+cp /tmp/rasax.nginx /tmp/ssl.conf /opt/bitnami/nginx/conf/conf.d
 
 if [[ ! -e "/opt/bitnami/certs/fullchain.pem" ]] || [[ ! -e "/opt/bitnami/certs/privkey.pem" ]]
 then
